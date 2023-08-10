436caf4a9312370116c449cfb8f1765e2283c0c5
aidityasadhakim <aidityasadhakim250@gmail.com>
2023-08-10 16:18:56 +0800

    Change config and databases

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ date_default_timezone_set('Asia/Makassar');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://192.168.1.4/sales/';
-$config['base_url'] = 'http://127.0.0.1/sales/';
+$config['base_url'] = 'http://192.168.1.4/sales/';
+// $config['base_url'] = 'http://127.0.0.1/sales/';
 
 
 /*
